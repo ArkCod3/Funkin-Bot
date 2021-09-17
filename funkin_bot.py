4d27df870ce4cd067b19547f72249195d4729018
ArkCod3 <nono95y@gmail.com>
2021-09-17 19:23:50 -0400

    Refactored funkin_bot.py to allow easier export of functions to configuration script.

--- funkin_bot.py
+++ funkin_bot.py
@@ -16,33 +16,17 @@ import os
 pyautogui.PAUSE = 0
 pydirectinput.PAUSE = 0
 
-filename = input('Enter configuration name: ')
-path = os.path.join('configs',filename+'.json')
-with open(path) as f:
-    settings = json.load(f)
-
-print(settings)
-
-LEFT_COLOR = settings['left_color']
-DOWN_COLOR = settings['down_color']
-UP_COLOR = settings['up_color']
-RIGHT_COLOR = settings['right_color']
-GRAY_COLOR = [173, 163, 135]
-WHITE_COLOR = [255, 255, 255]
-
-LEFT_HOLD = settings['left_hold']
-DOWN_HOLD = settings['down_hold']
-UP_HOLD = settings['up_hold']
-RIGHT_HOLD = settings['right_hold']
-
 class Arrow():
     '''Class to track data for each arrow and react accordingly.
     Takes a position tuple (x,y), a BGR 8-bit color for the
     incoming arrows, a key character to press, and the arrow held
     color after a succesful hit.'''
 
+    GRAY_COLOR = [173, 163, 135]
+    WHITE_COLOR = [255, 255, 255]
+
     def __init__(self, position, color, key, hold_color):
-        self.position = position
+        self.rel_pos = position
         self.color = color
         self.key = key
         self.was_hit = True
@@ -50,11 +34,11 @@ class Arrow():
         self.hold_color = hold_color
 
     def update_arrow_state(self, img):
-        read_color = list(img[self.position[1]][self.position[0]][:3])
-        high_color = list(img[self.position[1]-offset_y][self.position[0]][:3])
+        read_color = list(img[self.rel_pos[1]][self.rel_pos[0]][:3])
+        high_color = list(img[self.rel_pos[1]-offset_y][self.rel_pos[0]][:3])
         #higher position allows recognition of consecutive, fast notes
 
-        if read_color != self.color or high_color == WHITE_COLOR or high_color == self.hold_color:
+        if read_color != self.color or high_color == Arrow.WHITE_COLOR or high_color == self.hold_color:
             self.was_hit = True
             #was_hit prevents multiple presses for the same note
 
@@ -67,14 +51,58 @@ class Arrow():
         return None
 
     def update_position(self, new_pos):
-        self.position = new_pos
+        self.rel_pos = new_pos
         return None
 
     def get_abs_pos(self):
         print(f'Place cursor on the arrow for key "{self.key}".')
         print('Press "shift" to confirm position.')
         keyboard.wait("shift")
-        return pyautogui.position()
+        self.abs_pos = pyautogui.position()
+        return None
+
+def make_monitor(abs_positions):
+    '''Intakes a list of game arrow absolute positions in 
+    (x,y) format. Outputs the corresponding mss() monitor 
+    for image capture, and the relative positions of all 
+    arrows within that monitor.'''
+
+    #Use absolute arrow positions to construct playing area
+    raw_width = abs_positions[-1][0]-abs_positions[0][0]
+    box_width = int(raw_width*1.3)
+    box_height = int(box_width*0.24)
+    box_left = int(abs_positions[0][0]-raw_width*0.15)
+    box_top = int(abs_positions[0][1]-box_height*11/16)
+    offset_y = int(box_height*0.4)
+
+    monitor_box = {'top':box_top, 'left': box_left,
+    'width': box_width, 'height':box_height}
+
+    #Redefine relative arrow positions within playing area
+    rel_pos0 = (int(raw_width*0.15), int(box_height*11/16))
+
+    rel_positions = [rel_pos0]
+    for abs_pos in abs_positions[1:]:
+        vector_x = abs_pos[0] - abs_positions[0][0]
+        vector_y = abs_pos[1] - abs_positions[0][1]
+        rel_positions.append((rel_pos0[0]+vector_x,rel_pos0[1]+vector_y))
+
+    return monitor_box, rel_positions
+
+filename = input('Enter configuration name: ')
+path = os.path.join('configs',filename+'.json')
+with open(path) as f:
+    settings = json.load(f)
+
+LEFT_COLOR = settings['left_color']
+DOWN_COLOR = settings['down_color']
+UP_COLOR = settings['up_color']
+RIGHT_COLOR = settings['right_color']
+
+LEFT_HOLD = settings['left_hold']
+DOWN_HOLD = settings['down_hold']
+UP_HOLD = settings['up_hold']
+RIGHT_HOLD = settings['right_hold']
 
 left_arrow = Arrow((0,0),LEFT_COLOR,'a',LEFT_HOLD)
 down_arrow = Arrow((0,0),DOWN_COLOR,'s',DOWN_HOLD)
@@ -83,30 +111,14 @@ right_arrow = Arrow((0,0),RIGHT_COLOR,'d', RIGHT_HOLD)
 
 arrows = [left_arrow, down_arrow, up_arrow, right_arrow]
 
-abs_positions = []
+absolute_positions = []
 for arrow in arrows:
-    abs_positions.append(arrow.get_abs_pos())
+    arrow.get_abs_pos()
+    absolute_positions.append(arrow.abs_pos)
 print('Arrow positions defined.')
 
-#Use absolute arrow positions to construct playing area
-raw_width = abs_positions[-1][0]-abs_positions[0][0]
-box_width = int(raw_width*1.3)
-box_height = int(box_width*0.24)
-box_left = int(abs_positions[0][0]-raw_width*0.15)
-box_top = int(abs_positions[0][1]-box_height*11/16)
-offset_y = int(box_height*0.4)
-
-arrow_box = {'top':box_top, 'left': box_left,
-'width': box_width, 'height':box_height}
-
-#Redefine relative arrow positions within playing area
-rel_pos0 = (int(raw_width*0.15), int(box_height*11/16))
-
-rel_positions = [rel_pos0]
-for abs_pos in abs_positions[1:]:
-    vector_x = abs_pos[0] - abs_positions[0][0]
-    vector_y = abs_pos[1] - abs_positions[0][1]
-    rel_positions.append((rel_pos0[0]+vector_x,rel_pos0[1]+vector_y))
+arrow_box, rel_positions = make_monitor(absolute_positions)
+offset_y = int(arrow_box['height']*0.4)
 
 for arrow, rel_position in zip(arrows,rel_positions):
     arrow.update_position(rel_position)
